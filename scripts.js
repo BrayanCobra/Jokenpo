7ca4da37c2cd05ed4a5530c4ea3b00615bb25610
BrayanCobra <brayancobrafr@gmail.com>
2026-01-16 17:13:28 -0300

    Dev Jokenpo

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const result = document.querySelector(".result")
+const yourscore = document.querySelector("#yourscore")
+const machinescore = document.querySelector("#machinescore")
+const reaction = document.querySelector("#reaction")
+
+let humanscorenumber = 0
+let machinescorenumber = 0
+
+const playhuman = (humanchoice) => {
+
+    playthegame(humanchoice, playmachine())
+}
+
+const playmachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomnumber = Math.floor(Math.random() * 3)
+
+    return choices[randomnumber]
+}
+
+const playthegame = (human, machine) => {
+    console.log('humano: ' + human + ' Máquina: ' + machine)
+
+    if (human === machine) {
+        result.innerHTML = "Deu empate!"
+        reaction.src = "loled-serioused-emoji-silly-emoji.mp4"
+    } else if (
+        (human === 'paper' && machine === 'rock') ||
+        (human === 'rock' && machine === 'scissors') ||
+        (human === 'scissors' && machine === 'paper')
+    ) {
+        humanscorenumber++
+        yourscore.innerHTML = humanscorenumber
+        result.innerHTML = "Você ganhou!"
+        reaction.src = "b838c81e54b62a432135e656a4d5dfcb.mp4"
+    } else {
+        machinescorenumber++
+        machinescore.innerHTML = machinescorenumber
+        result.innerHTML = "Você perdeu para a máquina!"
+        reaction.src = "f14f214b3e79b325c7c0dfd5666460be.mp4"
+    }
+}
